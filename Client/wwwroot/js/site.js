6b0d24866209dd0c7eefb33d1c8637741cdc93f8
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-08-09 15:09:22 -0700

    JS added for animations

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -1,3 +1,21 @@
 ﻿function printInvoke() {
     window.print();
 };
+
+const observer5 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            return;
+        }
+    });
+});
+
+const hiddenElementsLeft = document.querySelectorAll('.hidden-from-left');
+hiddenElementsLeft.forEach((el) => observer5.observe(el));
+
+const hiddenElementsTop = document.querySelectorAll('.hidden-from-top');
+hiddenElementsTop.forEach((el) => observer5.observe(el));
+
+const hiddenElementsTop = document.querySelectorAll('.hidden-from-right');
+hiddenElementsTop.forEach((el) => observer5.observe(el));
